c404e6510b059070c467dc8ba0daa9373c202560
sgr98 <60283755+sgr98@users.noreply.github.com>
2022-04-17 10:39:24 +0530

    Added appropriate error examples to Preprocessor

--- project_history/Preprocessor/sample.cpp
+++ project_history/Preprocessor/sample.cpp
@@ -6,10 +6,19 @@ using namespace std;
 
 // g++ sample.cpp preprocessor.cpp -o sample
 // FileName: ./exmp/arithmetic.xvgs
+// error_arithmetic_1.xvgs
 
-int main () {
-    string fileName = "./exmp/arithmetic.xvgs";
+int main(int argc, char *argv[]) {
+    string fileName = "./exmp/arithmetic_1.xvgs";
     string processedFileName = "target.xvgs";
+
+    if(argc > 2) {
+        fileName = string(argv[1]);
+        processedFileName = string(argv[2]);
+    }
+    else if(argc > 1) {
+        fileName = string(argv[1]);
+    }
 
     Preprocessor preprocessor;
     string preProcessedText = preprocessor.processFileText(fileName, processedFileName);
