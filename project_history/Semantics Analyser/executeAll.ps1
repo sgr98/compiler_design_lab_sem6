5031c4acaff555489ee596275f517da35353a710
sgr98 <60283755+sgr98@users.noreply.github.com>
2022-04-24 15:17:17 +0530

    Added script for executing all example files together

---
+++ project_history/Semantics Analyser/executeAll.ps1
@@ -0,0 +1,46 @@
+Write-Host "||||||||||||||||||||||||||||||||||||||"
+Write-Host "NON ERROR SCRIPTS"
+
+Write-Host ""
+Write-Host "arithmetic.xvgs"
+./parser ./../../example_programs/our_language/Expression/arithmetic.xvgs
+Write-Host ""
+Write-Host "character.xvgs"
+./parser ./../../example_programs/our_language/Expression/character.xvgs
+Write-Host ""
+Write-Host "logical.xvgs"
+./parser ./../../example_programs/our_language/Expression/logical.xvgs
+Write-Host ""
+Write-Host "e.xvgs"
+./parser ./../../example_programs/our_language/Expression/e.xvgs
+Write-Host ""
+Write-Host "e1.xvgs"
+./parser ./../../example_programs/our_language/Expression/e1.xvgs
+Write-Host ""
+Write-Host "cond1.xvgs"
+./parser ./../../example_programs/our_language/Conditional/cond1.xvgs
+Write-Host ""
+Write-Host "cond2.xvgs"
+./parser ./../../example_programs/our_language/Conditional/cond2.xvgs
+Write-Host ""
+Write-Host "cond3.xvgs"
+./parser ./../../example_programs/our_language/Conditional/cond3.xvgs
+Write-Host ""
+Write-Host "cond4.xvgs"
+./parser ./../../example_programs/our_language/Conditional/cond4.xvgs
+Write-Host ""
+Write-Host "func1.xvgs"
+./parser ./../../example_programs/our_language/Function/func1.xvgs
+Write-Host ""
+Write-Host "func2.xvgs"
+./parser ./../../example_programs/our_language/Function/func2.xvgs
+Write-Host ""
+Write-Host "loop.xvgs"
+./parser ./../../example_programs/our_language/Loop/loop.xvgs
+
+Write-Host ""
+Write-Host "||||||||||||||||||||||||||||||||||||||"
+Write-Host "ERROR SCRIPTS"
+Write-Host ""
+Write-Host "arithmetic_err1.xvgs"
+./parser ./../../example_programs/our_language/Expression/arithmetic_err1.xvgs
